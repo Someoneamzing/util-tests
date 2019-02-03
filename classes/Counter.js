6e2dd5121ee8533cabbb64abdf8bcb7478bab25b
Jacob McEwan <jacob@mchew.com>
2019-02-03 15:52:09 +1100

    Fix to work with the new properties system

--- classes/Counter.js
+++ classes/Counter.js
@@ -15,7 +15,7 @@ class Counter extends NetworkWrapper(Building, list) {
     switch(SIDE){
       case ConnectionManager.SERVER:
         super.update();
-        Math.random() > 0.9?this.counter ++:'';
+        Math.random() > 0.9?this.counter = (this.counter + 1) % this.resetTime:'';
 
         break;
 
--- guis.js
+++ guis.js
@@ -3,5 +3,5 @@ const {GUI, GUIElement, GUITextBox, GUIProgressBar} = require('electron-game-uti
 let counter = new GUI('counter', 300, 400, connection.server);
 
 counter.registerElements = function(){
-  this.addElement(new GUIProgressBar({name: 'number', max: 'resetTime', progress: 'counter', w: 100, h: 20, x: 30, y: 20}))
+  this.addElement(new GUIProgressBar({name: 'number', max: new GUIElement.Property({property: "resetTime"}), progress: new GUIElement.Property({property: "counter"}), w: 100, h: 20, x: 30, y: 20}))
 }
